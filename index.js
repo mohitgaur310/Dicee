e806fca5dbe83dd9fd135f0c987870993ef9e921
Mohit kumar Gaur <115886566+mohitgaur310@users.noreply.github.com>
2023-04-13 14:06:49 +0530

    added a condition in jsFile

--- index.js
+++ index.js
@@ -9,8 +9,10 @@
     var imageRand1="images/dice"+num1+".png";
     document.querySelectorAll("img")[1].setAttribute("src",imageRand1);
 
-    if(imageRand>imageRand1){
+    if(num>num1){
         document.querySelector("h1").innerText="Player1 is Winner";
-    }else{
+    }else if(num<num2){
         document.querySelector("h1").innerText="Player2 is Winner";
+    }else{
+        document.querySelector("h1").innerText="Draw";
     }
